bc9789f567274a0c1083db6930ac7fa0158c18cd
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2021-03-17 22:30:36 -0300

    Implementação do menu e rodape

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { EntraComponent } from './entra/entra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    EntraComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +22,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+© 2021 GitHub, Inc.
